808630d61580921f80b5bf409a972dd6ba2f4621
casinilorenzo <casini.lorenzo@franchettisalviani.net>
2026-04-12 00:31:43 +0200

    creazione classe monitor

---
+++ src/threadrelay/Monitor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package threadrelay;
+
+/**
+ *
+ * @author loren
+ */
+public class Monitor {
+
+    private int currentRunner = 1;
+    private boolean paused = false;
+    private boolean stopped = false;
+
+    
+}
