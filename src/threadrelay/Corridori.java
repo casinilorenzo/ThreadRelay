5de0334d1db986dc78ef6fdc29937883d03fcaee
casinilorenzo <casini.lorenzo@franchettisalviani.net>
2026-04-12 00:37:14 +0200

    attributi e costruttore classe threads

--- src/threadrelay/Corridori.java
+++ src/threadrelay/Corridori.java
@@ -8,6 +8,19 @@ package threadrelay;
  *
  * @author loren
  */
-public class Corridori {
-    
+public class Corridori implements Runnable {
+    private final int id;
+    private final Monitor monitor;
+    private final int[] contatori;
+    private int velocita;
+    private boolean finito = false;
+
+    public Corridori(int id, Monitor monitor, int[] contatori, int velocita) {
+        this.id       = id;
+        this.monitor  = monitor;
+        this.contatori = contatori;
+        this.velocita  = velocita;
+    }
+
 }
+
